6bc827d517e760a431ac92f9fc2df1b10fffa475
Donald Cyrille <dcyrile@gmail.com>
2016-02-03 14:33:42 -0500

    Add and create action article

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'page/home' => 'page#home'
   get 'page/about' => 'page#about'
   root 'page#home'
+  resources :articles
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
